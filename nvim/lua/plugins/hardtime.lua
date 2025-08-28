0cafd9d59d264d6da82ab5cbef87a6b2928e17f8
assil-bouzaine <bouzaineassil015@gmail.com>
2025-08-28 18:09:40 +0100

    update

--- nvim/lua/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return { -- Plugin to prevent hard-to-recover mistakes by limiting certain actions.
-    'm4xshen/hardtime.nvim',
-    lazy = false,
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    opts = {
-      restriction_mode = 'block',
-      max_count = 100,
-    },
-  }
